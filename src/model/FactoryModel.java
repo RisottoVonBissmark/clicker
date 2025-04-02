56280e96c0f9f1c8562146c7d95ad2b66d989b17
helori_ollivier <helori.ollivier@free.fr>
2025-04-02 12:15:17 +0200

    added FactoryModel.java

---
+++ src/model/FactoryModel.java
@@ -0,0 +1,39 @@
+package model;
+
+public class FactoryModel {
+
+    double production;
+    double delay;
+    int upgradeValue;
+
+    public FactoryModel() {
+        this.production = 1;
+        this.delay = 1;
+        this.upgradeValue = 10;
+    }
+    
+    public double getProduction() {
+        return this.production;
+    }
+
+    public double getDelay() {
+        return this.delay;
+    }
+
+    public int getUpgradeValue() {
+        return this.upgradeValue;
+    }
+
+    public void setProduction(double newProduction) {
+        this.production = newProduction;
+    }
+
+    public void setDelay(double newDelay) {
+        this.delay = newDelay;
+    }
+
+    public void setUpgradeValue( int newUpgradeValue) {
+        this.upgradeValue = newUpgradeValue;
+    }
+
+}
